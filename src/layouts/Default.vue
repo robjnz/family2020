04acc8eab36fcb073f27671124c61d91ab421030
robin <robjnz@outlook.com>
2020-11-03 15:39:35 +0000

    new family2020 update

---
+++ src/layouts/Default.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <a class="visually-hidden" href="#main">Skip to content</a>
+
+    <div id="wrapper" class="wrapper pb-16 md:pb-0 flex flex-col relative min-h-screen bg-teal-200">
+
+      <HeaderPartial/>
+
+
+      <main id="main" class="main inner flex flex-1 flex-col py-10 lg:py-20">
+        <slot/>
+      </main>
+
+      <FooterPartial/>
+        
+    </div>
+
+    <ClientOnly>
+    <ResponsiveNav/>
+    </ClientOnly>
+    
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+import HeaderPartial from '~/layouts/partials/Header.vue'
+import FooterPartial from '~/layouts/partials/Footer.vue'
+import ResponsiveNav from '~/layouts/partials/ResponsiveNav.vue'
+
+export default {
+  script: [
+
+        {src: 'https://kit.fontawesome.com/e3c70d4fc5.js'}
+      ],
+  components: {
+    HeaderPartial,
+    FooterPartial,
+    ResponsiveNav
+  }
+}
+</script>
+
+
+<style>
+
+.layout {
+  max-width    : auto;
+  margin       : 0 auto;
+  padding-left : 2px;
+  padding-right: 2px;
+  background   : #242943;
+  color        : #ebf8ff;
+  font-size    : 15;
+
+}
+
+.work-card {
+            box-shadow   : 0px 0px 20px rgba(0, 0, 0.1);
+            padding      : 1rem;
+            border-radius: 0.2rem;
+            color        : #44337a;
+            font-size    : medium;
+            font-family: Roberto;
+}
+#work-card {
+  box-shadow: 0px 0px 20px rgba(0, 0, 0.1);
+  background: #38b2ac;
+  font-family: roberto;
+  color: #1a202c;
+  font-size: 1.25rem;
+}
+#background{
+    background: #b2f5ea;
+}
+.postcode{
+color     : #68d391;
+
+}
+
+.event{
+color     : #4fd1c5;
+
+}
+
+.one{
+    color: #e53e3e;
+    
+  }
+
+
+</style>
---
+++ src/pages/Europe.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout>
+    <div id="background" class="container m-auto">
+      <h1 class="ml-8 mt-3 text-4xl text-teal-900">
+        <span class="text-teal-900">
+          <i class="fas fa-theater-masks"></i>
+        </span>
+        Family History Resourses For Europe
+      </h1>
+      
+      <br />
+
+      <div class="grid sm:flex">
+        <austria /><!--Austria-->
+        <belarus /><!--Belarus-->
+        <belgium /><!--Belgium-->
+
+      </div>
+
+     
+    </div>
+    <br />
+  </Layout>
+</template>
+
+<script>
+
+import austria from "@/components/Europe/austria";
+import belarus from "@/components/Europe/belarus";
+import belgium from "@/components/Europe/belgium";
+
+
+
+
+
+export default {
+  metaInfo: {
+    title: "Europe",
+    meta: [
+      {
+        name: "description",
+        content: "A to Z guide of what plays, that are showing at London theatres. Due to coronavirus London theatres are curently closed, new exciting shows coming soon.",
+      },
+       [
+
+        {src: 'https://kit.fontawesome.com/e3c70d4fc5.js'}
+      ],
+    ],
+  },
+  components: {
+    austria,
+    belarus,
+    belgium,
+    
+  },
+};
+</script>
+
+<style></style>
